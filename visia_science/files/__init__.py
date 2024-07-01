4f86923ad7379e523f9d70b8217aab6ab8862425
Jose M. Ramirez <moscaelectronica@gmail.com>
2024-07-01 17:48:20 +0200

    feat(Files Module): Add a module to handle files

---
+++ visia_science/files/__init__.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+
+def is_file_a_valid_ext(path: str, extension: str = None) -> bool:
+    if not os.path.exists(path):
+        return False
+
+    if extension is not None:
+        if not path.endswith(extension):
+            return False
+
+    return True
+
+
+def load_json_as_dict(path: str) -> dict:
+
+    if not is_file_a_valid_ext(path, ".json"):
+        raise ValueError(f"File {path} is not a valid JSON file")
+
+    try:
+        with open(path, "r") as file:
+            json_as_dict = json.load(file)
+    except Exception as e:
+        message = f"Error loading JSON file: {e}"
+        json_as_dict = {}
+
+    return json_as_dict
