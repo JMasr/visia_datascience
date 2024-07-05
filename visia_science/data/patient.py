3b900ddb269f3322948fd2dc1b205a83a38438ae
José M. Ramírez <moscaelectronica@gmail.com>
2024-07-05 19:15:44 +0200

    feat(Data Module): Add Enum types support for Patient class

--- visia_science/data/patient.py
+++ visia_science/data/patient.py
@@ -1,33 +1,76 @@
 from datetime import date
 from enum import Enum
 
+import pandas as pd
+from pandas import DataFrame
 from pydantic import BaseModel
+
+from visia_science.data.questionary import VisiaQuestionary
 
 
 class VisiaGroup(Enum):
-    GROUP_1: str = "G1-clinical S"
-    GROUP_2: str = "G2-clinical non-S"
+    GROUP_1: str = "G1-clínico-S"
+    GROUP_2: str = "G2-clínico-N"
     GROUP_3: str = "G3-general"
+    UNK: str = "UNK"
+
+
+def return_visia_group(input_value: str):
+    if input_value.lower() in VisiaGroup.GROUP_1.value.lower():
+        return VisiaGroup.GROUP_1
+    elif input_value.lower() in VisiaGroup.GROUP_2.value.lower():
+        return VisiaGroup.GROUP_2
+    elif input_value.lower() in VisiaGroup.GROUP_3.value.lower():
+        return VisiaGroup.GROUP_3
+    else:
+        return VisiaGroup.UNK
 
 
 class Education(Enum):
-    PRIMARY: str = "primary"
-    SECONDARY: str = "secondary"
-    BACHELOR: str = "bachelor"
-    PROFESSIONAL: str = "professional"
+    PRIMARY: str = "1-Primaria"
+    SECONDARY: str = "2-Secundaria"
+    BACHELOR: str = "3-Bachiller-ciclo-medio-FP-básica"
+    PROFESSIONAL: str = "4-Certificado-profesional"
+    UNK: str = "UNK"
+
+
+def return_education_level(input_value: str):
+    if input_value.lower() in Education.PRIMARY.value.lower():
+        return Education.PRIMARY
+    elif input_value.lower() in Education.SECONDARY.value.lower():
+        return Education.SECONDARY
+    elif input_value.lower() in Education.BACHELOR.value.lower():
+        return Education.BACHELOR
+    elif input_value.lower() in Education.PROFESSIONAL.value.lower():
+        return Education.PROFESSIONAL
+    else:
+        return Education.UNK
 
 
 class Sex(Enum):
-    FEMALE: str = "female"
-    MALE: str = "male"
+    FEMALE: str = "mujer"
+    MALE: str = "hombre"
+    UNK: str = "UNK"
+
+
+def return_sex(input_value: str):
+    if input_value.lower() in Sex.MALE.value.lower():
+        return Sex.MALE
+    elif input_value.lower() in Sex.FEMALE.value.lower():
+        return Sex.FEMALE
+    else:
+        return Sex.UNK
 
 
 class Patient(BaseModel):
     id: str
     age: int
     sex: Sex
-    birth_date: date
     education_level: Education
     clinical_group: VisiaGroup
-    extra_info: dict = None
+    diagnosis: str
+    treatment: str
+    saliva_sample: bool
 
+    class Config:
+        arbitrary_types_allowed = True
