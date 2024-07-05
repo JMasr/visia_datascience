7688ade56b47d1507a23b93847afa343a0004e0d
José M. Ramírez <moscaelectronica@gmail.com>
2024-07-05 19:18:06 +0200

    feat(Files Module): Add a function to save python dict as JSON file

--- visia_science/files/__init__.py
+++ visia_science/files/__init__.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from visia_science import app_logger
+
 
 def is_file_a_valid_ext(path: str, extension: str = None) -> bool:
     if not os.path.exists(path):
@@ -14,7 +16,6 @@ def is_file_a_valid_ext(path: str, extension: str = None) -> bool:
 
 
 def load_json_as_dict(path: str) -> dict:
-
     if not is_file_a_valid_ext(path, ".json"):
         raise ValueError(f"File {path} is not a valid JSON file")
 
@@ -25,3 +26,15 @@ def load_json_as_dict(path: str) -> dict:
         raise RuntimeError(f"Error loading JSON file: {e}")
 
     return json_as_dict
+
+
+def save_dict_as_json(data: dict, path: str) -> None:
+    if os.path.dirname(path) != "" and not os.path.exists(os.path.dirname(path)):
+        app_logger.warning(f"Creating directory {os.path.dirname(path)}")
+        os.makedirs(os.path.dirname(path))
+
+    try:
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4)
+    except Exception as e:
+        raise RuntimeError(f"Error saving JSON file: {e}")
